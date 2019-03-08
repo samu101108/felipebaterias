079c5615e4d06539eb7101dbe4e021b823408ce8
Samuel Barbosa <samu101108@gmail.com>
2019-03-07 22:54:41 -0300

    Imports dos components

--- felipebaterias/src/App.js
+++ felipebaterias/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import Cabecalho from './components/cabecalho'
-import Propaganda from './components/propaganda'
-import Produtos from './components/produtos'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Solicitar from './components/Solicitar/Solicitar'
+import Sobre from './components/Sobre/Sobre'
+import Baterias from './components/Baterias/Baterias'
+import Contato from './components/Contato/Contato'
+import Footer from './components/Footer/Footer'
+
+
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-       <Cabecalho/>
-       <Propaganda/>
-       <Produtos/>
+       <Header/>
+       <Hero/>
+       <Solicitar/>
+       <Sobre/>
+       <Baterias/>
+       <Contato/>
+       <Footer/>
       </div>
     )
   }
